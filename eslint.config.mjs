b3d28f52c54cb209444483f95d1502280b7096fc
b3t0247 <wb2k9@yahoo.com>
2025-06-12 15:53:04 -0700

    Configure project with ESLint, Prettier, TypeScript, ShadCN UI, Next-Themes, and Dark Mode Toggle

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,34 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next",
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      // '@typescript-eslint/explicit-function-return-type': ['warn'],
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "all",
+          semi: true,
+          tabWidth: 2,
+          singleQuote: false,
+          printWidth: 80,
+          endOfLine: "auto",
+          arrowParens: "always",
+          plugins: ["prettier-plugin-tailwindcss"],
+        },
+        { usePrettierrc: false },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
